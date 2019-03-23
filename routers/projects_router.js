8dc992b83ca15336b5c2002ef90f645e67c7d321
Leila <nb.leila10@gmail.com>
2019-03-23 11:53:20 -0500

    Implement POST for adding projects

---
+++ routers/projects_router.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const router = express.Router()
+
+const projectsDB = require("../data/projects/projects_db")
+
+router.post("/", (req, res) => {
+  const { proj_name, proj_description, proj_completed } = req.body
+  console.log(proj_name, proj_description, proj_completed)
+  if (!proj_name || !proj_description || proj_completed === null)
+    res.status(400).json({
+      errorMessage:
+        "Please provide the project name, description, and completed flag."
+    })
+  else
+    projectsDB
+      .addProject(req.body)
+      .then(projectId => res.status(201).json(projectId))
+      .catch(err =>
+        res.status(500).json({
+          error: "There was an error while saving the project to the database"
+        })
+      )
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -2,14 +2,18 @@ const express = require("express")
 const helmet = require("helmet")
 const logger = require("morgan")
 
+const projectsRouter = require("./routers/projects_router")
+
 const server = express()
 
 server.use(express.json())
 server.use(helmet())
 server.use(logger("dev"))
 
+server.use("/api/projects", projectsRouter)
+
 server.get("/", (req, res) => {
-  res.status(200).json("Welcome to -- Sprint Challenge - RDMS? -- ")
+  res.status(200).json("Welcome to -- Sprint Challenge - RDMS -- ")
 })
 
 module.exports = server
