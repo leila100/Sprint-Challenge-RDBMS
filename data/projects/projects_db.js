20c0a4f6524a9e12bc1ac81e92ae12e373d35e96
Leila <nb.leila10@gmail.com>
2019-03-23 12:39:25 -0500

    Implement GET for retrieving a project by id
    returns the information of the project and an array of the actions associated with it
    Return the completed flag as true or false (not 1 or 0)

--- data/projects/projects_db.js
+++ data/projects/projects_db.js
@@ -3,11 +3,26 @@ const knexConfig = require("../../knexfile.js")
 const db = knex(knexConfig.development)
 
 module.exports = {
-  addProject
+  addProject,
+  getById
 }
 
 function addProject(project) {
   return db("projects")
     .insert(project)
     .then(ids => ({ id: ids[0] }))
+}
+
+function getById(id) {
+  return db
+    .select(
+      "projects.id as project_id",
+      "proj_name",
+      "proj_description",
+      "proj_completed",
+      "actions.*"
+    )
+    .from("projects")
+    .innerJoin("actions", "actions.project_id", "projects.id")
+    .where({ "projects.id": Number(id) })
 }
--- routers/projects_router.js
+++ routers/projects_router.js
@@ -21,4 +21,38 @@ router.post("/", (req, res) => {
       )
 })
 
+router.get("/:id", (req, res) => {
+  const { id } = req.params
+  projectsDB
+    .getById(id)
+    .then(project => {
+      if (project) {
+        const actions = project.map(action => {
+          return {
+            id: action.id,
+            description: action.action_description,
+            notes: action.notes,
+            completed: action.action_completed ? true : false
+          }
+        })
+        const proj = {
+          id: project[0].project_id,
+          name: project[0].proj_name,
+          description: project[0].proj_description,
+          completed: project[0].proj_completed ? true : false,
+          actions: actions
+        }
+        res.status(200).json(proj)
+      } else
+        res.status(400).json({
+          errorMessage: "Please provide a valid id for the project."
+        })
+    })
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: "The project information could not be retrieved." })
+    )
+})
+
 module.exports = router
